449d6dade7d959280f88f166a4af61af1f7677e2
Fluriko <flurikochi@gmail.com>
2019-05-16 22:37:07 +0300

    RegServletTest

--- src/main/test/mate/academy/servlets/RegServletTest.java
+++ src/main/test/mate/academy/servlets/RegServletTest.java
@@ -3,6 +3,7 @@ package mate.academy.servlets;
 import mate.academy.database.user.UserDao;
 import mate.academy.database.user.UserDaoHib;
 import mate.academy.model.User;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -13,9 +14,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Optional;
 
 public class RegServletTest {
-    private static final UserDao USER_DAO = new UserDaoHib();
     RegServlet regServlet;
 
     @Mock
@@ -33,39 +34,71 @@ public class RegServletTest {
         regServlet = new RegServlet();
     }
 
-    @Test
-    public void doPostHello() throws IOException, ServletException {
-        Mockito.when(request.getParameter("name")).thenReturn("alpha");
-        Mockito.when(request.getParameter("password")).thenReturn("111qqq");
-        Mockito.when(request.getRequestDispatcher("index.jsp")).thenReturn(requestDispatcher);
-        regServlet.doPost(request, response);
-        Mockito.verify(request, Mockito.times(1)).getRequestDispatcher("index.jsp");
+    @After
+    public void clean() {
+        UserDao userDao = new UserDaoHib();
+        Optional<User> optionalUser = userDao.getUserByLogin("111111");
+//        optionalUser.ifPresent((o) -> userDao.removeUser(o));
+        if (optionalUser.isPresent()) {
+            userDao.removeUser(optionalUser.get());
+        }
     }
 
     @Test
-    public void doPostExist() throws IOException, ServletException {
-        User user = new User("betta", "111qqq", "bettaa@gmail.com");
-        USER_DAO.addUser(user);
-        Mockito.when(request.getParameter("name")).thenReturn("betta");
-        Mockito.when(request.getParameter("password")).thenReturn("111qqq");
-        Mockito.when(request.getRequestDispatcher("registration.jsp")).thenReturn(requestDispatcher);
+    public void doPostAdd() throws IOException, ServletException {
+        Mockito.when(request.getParameter("login")).thenReturn("111111");
+        Mockito.when(request.getParameter("password")).thenReturn("111111");
+        Mockito.when(request.getParameter("mail")).thenReturn("test11@gmail.com");
+        Mockito.when(request.getRequestDispatcher("/index.jsp")).thenReturn(requestDispatcher);
         regServlet.doPost(request, response);
-        Mockito.verify(request, Mockito.times(1)).getRequestDispatcher("registration.jsp");
+        Mockito.verify(request, Mockito.times(1)).getRequestDispatcher("/index.jsp");
     }
 
     @Test
-    public void doPostPass() throws IOException, ServletException {
-        Mockito.when(request.getParameter("name")).thenReturn("gamma");
+    public void doPostExistLog() throws IOException, ServletException {
+        Mockito.when(request.getParameter("login")).thenReturn("fluriko");
+        Mockito.when(request.getParameter("password")).thenReturn("222222");
+        Mockito.when(request.getParameter("mail")).thenReturn("newuniq@gmail.com");
+        Mockito.when(request.getRequestDispatcher("/registration.jsp")).thenReturn(requestDispatcher);
+        regServlet.doPost(request, response);
+        Mockito.verify(request, Mockito.times(1)).getRequestDispatcher("/registration.jsp");
+    }
+
+    @Test
+    public void doPostExistMail() throws IOException, ServletException {
+        Mockito.when(request.getParameter("login")).thenReturn("somerandomlogin");
+        Mockito.when(request.getParameter("password")).thenReturn("222222");
+        Mockito.when(request.getParameter("mail")).thenReturn("fluricode@gmail.com");
+        Mockito.when(request.getRequestDispatcher("/registration.jsp")).thenReturn(requestDispatcher);
+        regServlet.doPost(request, response);
+        Mockito.verify(request, Mockito.times(1)).getRequestDispatcher("/registration.jsp");
+    }
+
+    @Test
+    public void doPostIncorrectPass() throws IOException, ServletException {
+        Mockito.when(request.getParameter("login")).thenReturn("222222");
         Mockito.when(request.getParameter("password")).thenReturn("1");
-        Mockito.when(request.getRequestDispatcher("registration.jsp")).thenReturn(requestDispatcher);
+        Mockito.when(request.getParameter("mail")).thenReturn("test22@gmail.com");
+        Mockito.when(request.getRequestDispatcher("/registration.jsp")).thenReturn(requestDispatcher);
         regServlet.doPost(request, response);
-        Mockito.verify(request, Mockito.times(1)).getRequestDispatcher("registration.jsp");
+        Mockito.verify(request, Mockito.times(1)).getRequestDispatcher("/registration.jsp");
     }
 
     @Test
-    public void doPostLog() throws IOException, ServletException {
-        Mockito.when(request.getParameter("name")).thenReturn("g");
-        Mockito.when(request.getParameter("password")).thenReturn("111qqq");
+    public void doPostIncorrectLog() throws IOException, ServletException {
+        Mockito.when(request.getParameter("login")).thenReturn("g");
+        Mockito.when(request.getParameter("password")).thenReturn("222222");
+        Mockito.when(request.getParameter("mail")).thenReturn("test22@gmail.com");
+        Mockito.when(request.getRequestDispatcher("/registration.jsp")).thenReturn(requestDispatcher);
+        regServlet.doPost(request, response);
+        Mockito.verify(request, Mockito.times(1)).getRequestDispatcher("/registration.jsp");
+    }
+
+    @Test
+    public void doPostIncorrectMail() throws IOException, ServletException {
+        Mockito.when(request.getParameter("login")).thenReturn("222222");
+        Mockito.when(request.getParameter("password")).thenReturn("222222");
+        Mockito.when(request.getParameter("mail")).thenReturn("test2@test.com");
         Mockito.when(request.getRequestDispatcher("registration.jsp")).thenReturn(requestDispatcher);
         regServlet.doPost(request, response);
         Mockito.verify(request, Mockito.times(1)).getRequestDispatcher("registration.jsp");
